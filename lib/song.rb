170af0537d1d549a73c428ff9e4bf7a64090aca5
Allison Ricker <alliricker@gmail.com>
2020-10-17 20:10:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,13 +19,13 @@ class Song
     @@all
   end
 
-  #def self.reset_all
-  #  self.all.clear
-  #end
+  def self.reset_all
+    self.all.clear
+  end
 
-  #def self.count
-    #self.all.count
-  #end
+  def self.count
+    self.all.count
+  end
 
   def artist=(artist)
     @artist = artist
